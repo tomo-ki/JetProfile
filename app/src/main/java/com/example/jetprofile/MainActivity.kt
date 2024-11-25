79d57a0688437cd86029c3ae8324b560fb108064
tomoki-hara <tomoki_hara@r.recruit.co.jp>
2024-11-25 11:05:14 +0900

    var isShowDetail by remember { mutableStateOf(false) }追加

--- app/src/main/java/com/example/jetprofile/MainActivity.kt
+++ app/src/main/java/com/example/jetprofile/MainActivity.kt
@@ -5,42 +5,34 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Email
-import androidx.compose.material.icons.filled.Favorite
-import androidx.compose.material.icons.filled.LocationOn
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.Divider
-import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.jetprofile.components.CompanySection
 import com.example.jetprofile.components.DetailSection
-import com.example.jetprofile.components.Label
 import com.example.jetprofile.ui.theme.JetProfileTheme
 
 class MainActivity : ComponentActivity() {
@@ -60,14 +52,14 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun MainContent(innerPadding: PaddingValues){
+fun MainContent(innerPadding: PaddingValues) {
     Column(
         horizontalAlignment = Alignment.CenterHorizontally,
         modifier = Modifier
             .padding(innerPadding)
             .padding(20.dp)
     ) {
-//                        プロフィール画像
+//      プロフィール画像
         Image(
             painter = painterResource(id = R.drawable.img_profile),
             contentDescription = "プロフィール",
@@ -76,7 +68,7 @@ fun MainContent(innerPadding: PaddingValues){
                 .clip(RoundedCornerShape(10.dp))
         )
         Spacer(modifier = Modifier.height(20.dp))
-//                        名前
+//      名前
         Text(
             text = "tomo-ki",
             color = Color.Gray,
@@ -84,13 +76,14 @@ fun MainContent(innerPadding: PaddingValues){
             fontWeight = FontWeight.ExtraBold
         )
         Spacer(modifier = Modifier.height(20.dp))
-//                        職業
+//      職業
         Text(text = "職業：Androidエンジニア", color = Color.Gray, fontSize = 16.sp)
         Spacer(modifier = Modifier.height(20.dp))
 //
         CompanySection()
         Spacer(modifier = Modifier.height(20.dp))
-//                            詳細表示ボタン
+//      詳細表示ボタン
+        var isShowDetail by remember { mutableStateOf(false) }
         Button(
             modifier = Modifier.fillMaxWidth(),
             colors = ButtonDefaults.buttonColors(
@@ -98,11 +91,14 @@ fun MainContent(innerPadding: PaddingValues){
                     0xFFF85F6A
                 )
             ),
-            onClick = {}) {
+            onClick = { isShowDetail = !isShowDetail }
+        ) {
             Text(text = "詳細を表示", color = Color.White)
         }
         Spacer(modifier = Modifier.height(20.dp))
-//                          趣味&居住地
-        DetailSection()
+//      趣味&居住地
+        if (isShowDetail) {
+            DetailSection()
+        }
     }
 }
