c0eeb280f2cded50c8644535c5e34c926296e3b3
tomoki-hara <tomoki_hara@r.recruit.co.jp>
2024-11-25 11:09:08 +0900

    文字色変更

--- app/src/main/java/com/example/jetprofile/components/CompanySection.kt
+++ app/src/main/java/com/example/jetprofile/components/CompanySection.kt
@@ -7,19 +7,19 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Email
-import androidx.compose.material3.Divider
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 @Composable
-fun CompanySection (){
+fun CompanySection() {
     Column(
         horizontalAlignment = Alignment.Start,
         modifier = Modifier.fillMaxWidth()
@@ -28,22 +28,27 @@ fun CompanySection (){
         Text(
             text = "株式会社Goooogle",
             fontSize = 26.sp,
-            fontWeight = FontWeight.Bold
+            fontWeight = FontWeight.Bold,
+            color = MaterialTheme.colorScheme.primary
         )
         Spacer(modifier = Modifier.height(10.dp))
 //      部署・グループ名
         Text(
             text = "DXカンパニー　テクノロジーグループ",
-            color = Color.Gray,
+            color = MaterialTheme.colorScheme.secondary,
             fontSize = 16.sp,
         )
         Spacer(modifier = Modifier.height(20.dp))
 //      Email
         Label(icon = Icons.Default.Email, text = "Email")
         Spacer(modifier = Modifier.height(10.dp))
-        Text(text = "example@mail.com", fontSize = 16.sp)
+        Text(
+            text = "example@mail.com",
+            fontSize = 16.sp,
+            color = MaterialTheme.colorScheme.secondary,
+        )
         Spacer(modifier = Modifier.height(5.dp))
-        Divider(
+        HorizontalDivider(
             modifier = Modifier.clip(RoundedCornerShape(1000.dp)),
             thickness = 2.dp
         )
